d3d8a2ec2e3bbdd504aa8f926295d641fb8b93b8
Jefferson Rago <jefferson.s.rago@gmail.com>
2017-06-08 12:12:56 -0300

    Adicionando exercicios da aula 4

--- cursos/src/br/com/caelum/contas/controller/ContaController.java
+++ cursos/src/br/com/caelum/contas/controller/ContaController.java
@@ -1,7 +1,10 @@
 package br.com.caelum.contas.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import br.com.caelum.contas.dao.ContaDAO;
 import br.com.caelum.contas.modelo.Conta;
@@ -21,6 +24,17 @@ public class ContaController {
 	@RequestMapping("/form")
 	public String form(){
 		return "conta/formulario";
+	}
+	
+	@RequestMapping("/listaContas")
+	public ModelAndView lista(){
+		ContaDAO dao = new ContaDAO();
+		List<Conta> contas = dao.lista();
+		
+		ModelAndView mv = new ModelAndView("conta/lista");
+		mv.addObject("contas", contas);
+
+		return mv;
 	}
 	
 }
